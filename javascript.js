c1e6473f726c94dbc0da0d30bbc4128675c6548b
JocelwithL <rellesjocel@agmail.com>
2023-02-21 08:13:19 +0800

    Update Javascript function, add html divs and styles with css

--- javascript.js
+++ javascript.js
@@ -1,52 +1,79 @@
+const options = ["rock", "paper", "scissors"];
 
-const options = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
-    return options[Math.floor(Math.random() * options.length)];
+     return options [Math.floor(Math.random() * options.length)];
+     
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a tie ${playerSelection} friends with ${computerSelection}`;
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors" ||
-             playerSelection === "scissors" && computerSelection === "paper" ||
-             playerSelection === "paper" && computerSelection === "rock") {
-                return `You win! ${playerSelection} beats ${computerSelection}`;
-             }
-    else if (playerSelection === "scissors" && computerSelection === "rock" ||
-            playerSelection === "paper" && computerSelection === "scissors" ||
-            playerSelection === "rock" && computerSelection === "paper") {
-                return `You loose! ${computerSelection} beats ${playerSelection}`;
-    }
-    else {
-                return "You did not enter your choice correctly";
-    }
-}
-
-function getPlayerChoice(){
-    let validatedInput = false;
-    while(validatedInput == false){
-        let choice = prompt("Enter you choice!");
-        if (choice == null) {
+function getPlayerChoice() {
+    let isInputValid = false
+    while(isInputValid === false) {
+        let playerChoice = prompt("Enter your choice!")
+        if(playerChoice === ""){
             continue;
         }
-        choice = choice.toLowerCase();
-        if(options.includes(choice)) {
-            validatedInput = true;
-            return choice;
+        else {
+            playerChoice = playerChoice.toLowerCase();
+            if(options.includes(playerChoice)) {
+                return playerChoice;
+            }
         }
+    }
+}
+
+function checkWinner(computerSelection, playerSelection) {
+    if (computerSelection == playerSelection) {
+        return "tie";
+    }
+    
+    else if(computerSelection == "rock" && playerSelection == "paper" ||
+        computerSelection == "scissors" && playerSelection == "rock" ||
+        computerSelection == "paper" && playerSelection == "scissors") {
+        return "player";
+    }
+    else {
+        return "computer"
+    }
+}
         
+function playRound(computerSelection, playerSelection) {
+    roundResult = checkWinner(computerSelection, playerSelection);
+    if (roundResult == "tie") {
+        return "It's a tie!"
+    }
+    else if (roundResult == "player"){
+        return "You win!"
+    }
+    else {
+        return "You lose!"
     }
 }
 
-function game () {
-    for (let i=0; i<5; i++) {
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
-        let playerSelection = getPlayerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(computerSelection, playerSelection));
+        if(checkWinner(computerSelection, playerSelection) == "player"){
+            playerScore++;
+        }
+        else if (checkWinner(computerSelection, playerSelection) == "computer") {
+            computerScore++;
+        }
     }
-    console.log("Game Over")
+    console.log("----------------")
+    if(playerScore > computerScore) {
+        console.log("You're the champion!")
+    }
+    else if(computerScore > playerScore) {
+        console.log("You're a loser")
+    }
+    else {
+        console.log("No one wins")
+    }
 }
 
-game();
+game()
