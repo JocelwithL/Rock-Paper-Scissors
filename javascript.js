32a110a32cfce519af2238fa8382fd9e15ecdf97
JocelwithL <rellesjocel@agmail.com>
2023-03-18 11:28:52 +0800

    update the app's UI, add button for the player selection

--- javascript.js
+++ javascript.js
@@ -1,26 +1,67 @@
 const options = ["rock", "paper", "scissors"];
-
+const buttons = document.querySelectorAll('button');
+const playerScoreEl = document.getElementById("player-score")
+const computerScoreEl = document.getElementById("computer-score")
+const resultPlaceholderEl = document.getElementById("result-placeholder")
 
 function getComputerChoice() {
      return options [Math.floor(Math.random() * options.length)];
-     
 }
 
-function getPlayerChoice() {
-    let isInputValid = false
-    while(isInputValid === false) {
-        let playerChoice = prompt("Enter your choice!")
-        if(playerChoice === ""){
-            continue;
-        }
-        else {
-            playerChoice = playerChoice.toLowerCase();
-            if(options.includes(playerChoice)) {
-                return playerChoice;
+
+let playerScore = 0;
+let computerScore = 0;
+let roundLength = 5;
+
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(){
+        const playerSelection = button.id
+        const computerSelection = getComputerChoice();
+        console.log(playRound(computerSelection,playerSelection))
+        
+        let winner = checkWinner(computerSelection, playerSelection)
+
+            if(winner === "player") {
+                playerScore++
+                playerScoreEl.innerHTML = playerScore;
+                resultPlaceholderEl.innerHTML = `You win! <br>Computer chose ${computerSelection}`
             }
+            else if (winner === "computer") {
+                computerScore++
+                computerScoreEl.innerHTML = computerScore;
+                resultPlaceholderEl.innerHTML = `You lose! <br>Computer chose ${computerSelection}`
+            }
+            else if (winner === "tie") {
+                resultPlaceholderEl.innerHTML = `It's a tie! <br>Computer chose ${computerSelection} too!`
+            }
+            
+            console.log (playerScore)
+            console.log (computerScore)
+            playerScoreEl.innerHTML = playerScore;
+            computerScoreEl.innerHTML = computerScore;
+
+        if(playerScore === roundLength) {
+            console.log(`You defeated the computer!`)
+            playerScore = 0;
+            computerScore = 0;
+            resultPlaceholderEl.innerHTML = `Congratulations! <br>You defeated the computer! <br> Play again!`
+            
         }
-    }
-}
+        else if (computerScore === roundLength) {
+            console.log("Awww! You have been defeated by the computer!")
+            playerScore = 0;
+            computerScore = 0;
+            resultPlaceholderEl.innerHTML = `Awww! <br>You have been defeated by the computer! <br> Play again!`
+        }
+        
+    })
+})
+
+
+
+
+
 
 function checkWinner(computerSelection, playerSelection) {
     if (computerSelection == playerSelection) {
@@ -37,43 +78,74 @@ function checkWinner(computerSelection, playerSelection) {
     }
 }
         
+
+
+
 function playRound(computerSelection, playerSelection) {
-    roundResult = checkWinner(computerSelection, playerSelection);
+    let roundResult = checkWinner(computerSelection, playerSelection);
     if (roundResult == "tie") {
-        return "It's a tie!"
+        return `It's a tie! ${computerSelection} is same as ${playerSelection}`
     }
     else if (roundResult == "player"){
-        return "You win!"
+        return `You win! ${playerSelection} is powerful than ${computerSelection}`
     }
     else {
-        return "You lose!"
+        return `You lose! ${playerSelection} is weaker than ${computerSelection}`
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(computerSelection, playerSelection));
-        if(checkWinner(computerSelection, playerSelection) == "player"){
-            playerScore++;
-        }
-        else if (checkWinner(computerSelection, playerSelection) == "computer") {
-            computerScore++;
-        }
-    }
-    console.log("----------------")
-    if(playerScore > computerScore) {
-        console.log("You're the champion!")
-    }
-    else if(computerScore > playerScore) {
-        console.log("You're a loser")
-    }
-    else {
-        console.log("No one wins")
-    }
-}
 
-game()
+
+
+
+// function getPlayerChoice() {
+//     let isInputValid = false
+//     while(isInputValid === false) {
+//         let playerChoice = prompt("Enter your choice!")
+//         if(playerChoice === ""){
+//             continue;
+//         }
+//         else {
+//             playerChoice = playerChoice.toLowerCase();
+//             if(options.includes(playerChoice)) {
+//                 return playerChoice;
+//             }
+//         }
+//     }
+// }
+
+
+
+
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let roundLength = 5;
+//     for (let i = 0; i < roundLength; i++){
+
+//         const playerSelection = getPlayerChoice();
+//         const computerSelection = getComputerChoice();
+//         console.log(playRound(computerSelection, playerSelection));
+//         if(checkWinner(computerSelection, playerSelection) == "player"){
+//             playerScore++;
+//         }
+//         else if (checkWinner(computerSelection, playerSelection) == "computer") {
+//             computerScore++;
+//         }
+//         else if (checkWinner(computerSelection, playerSelection) == "tie") {
+//             roundLength++;
+//         }
+//     }
+//     console.log("----------------")
+//     if(playerScore > computerScore) {
+//         console.log("You're the champion!")
+//     }
+//     else if(computerScore > playerScore) {
+//         console.log("You're a loser")
+//     }
+//     else {
+//         console.log("No one wins")
+//     }
+// }
+
+// game()
